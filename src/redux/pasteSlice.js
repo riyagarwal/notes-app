b43164feaa52f0b332e043e55ae0b73eee03f3c7
Riya Agarwal <riyagarwal23@gmail.com>
2025-05-16 19:18:07 +0530

    pasteSlice created

---
+++ src/redux/pasteSlice.js
@@ -0,0 +1,58 @@
+import { createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+
+const initialState = {
+  // fetch data from local storage. if not present, then keep an empty array
+  pastes: localStorage.getItem("pastes")
+    ? JSON.parse(localStorage.getItem("pastes"))
+    : [],
+};
+
+export const pasteSlice = createSlice({
+  name: "paste",
+  initialState,
+  reducers: {
+    addToPastes: (state, action) => {
+      const paste = action.payload;
+      state.pastes.push(paste);
+      //update local storage so the change is persistent across page reloads
+      localStorage.setItem("pastes", JSON.stringify(state.pastes));
+      toast.success("Paste created successfully!");
+    },
+
+    updateToPastes: (state, action) => {
+      const paste = action.payload;
+      const index = state.pastes.findIndex((item) => item._id === paste._id);
+
+      if (index >= 0) {
+        state.pastes[index] = paste;
+        localStorage.setItem("pastes", JSON.stringify(state.pastes)); //local storage only stores strings
+        toast.success("Paste updated");
+      }
+    },
+
+    resetAllPastes: (state) => {
+      state.pastes = [];
+      localStorage.removeItem("pastes");
+    },
+
+    removeFromPastes: (state, action) => {
+      const pasteId = action.payload;
+      console.log(pasteId);
+      const index = state.pastes.findIndex((item) => item._id === pasteId);
+
+      if(index >=0)  {
+        state.pastes.splice(index, 1);
+        localStorage.setItem("pastes", JSON.stringify(state.pastes));
+
+        toast.success("Paste deleted");
+      }
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addToPastes, updateToPastes, resetAllPastes, removeFromPastes } =
+  pasteSlice.actions;
+
+export default pasteSlice.reducer;
